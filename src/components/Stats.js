bbea0622fe5853bfff7842e363c93f834ff351ae
solominh <hoangminhftu2@gmail.com>
2017-06-22 13:19:41 +0700

    Fix style

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -5,14 +5,15 @@ export default class Stats extends Component {
   render() {
     return (
       <div className="stats">
-        <div className="right-box">
-          <span className="right-symbol">&#10004;</span>
-          {this.props.right}
-        </div>
         <div className="wrong-box">
           <span className="wrong-symbol">&#10008;</span>
           {this.props.wrong}
         </div>
+        <div className="right-box">
+          <span className="right-symbol">&#10004;</span>
+          {this.props.right}
+        </div>
+
       </div>
     )
   }
