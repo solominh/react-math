e39011d608bf69df706bfdb38344a4dc6fdc38de
solominh <hoangminhftu2@gmail.com>
2017-06-27 12:57:32 +0700

    Add to version to DB key => update DB easily

--- src/db/localStorage.js
+++ src/db/localStorage.js
@@ -1,8 +1,11 @@
 
+const DB_VERSION = 1
+
+const getDBKey = (key) => `DB_VERSION_${DB_VERSION}_${key}`;
 
 export const loadState = (key) => {
   try {
-    let serializedState = localStorage.getItem(key)
+    let serializedState = localStorage.getItem(getDBKey(key))
     if (serializedState === null) {
       return undefined
     }
@@ -15,7 +18,7 @@ export const loadState = (key) => {
 export const saveState = (key, state) => {
   try {
     let serializedState = JSON.stringify(state)
-    localStorage.setItem(key, serializedState)
+    localStorage.setItem(getDBKey(key), serializedState)
   } catch (err) {
     console.log(err)
   }
