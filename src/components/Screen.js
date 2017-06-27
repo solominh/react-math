2b852fde6c01ab3864176c815e54162a35a6ec9a
solominh <hoangminhftu2@gmail.com>
2017-06-27 12:57:41 +0700

    clean up

--- src/components/Screen.js
+++ src/components/Screen.js
@@ -1,10 +1,9 @@
 import React, { Component } from 'react'
 
-export default class AddInRange20Screen extends Component {
+export default class Screen extends Component {
 
   render() {
     const { firstNumber, secondNumber, operator } = this.props.puzzle
-    console.log(this.props)
     const { currentAnswer } = this.props
 
     const text = `${firstNumber} ${operator} ${secondNumber} = ${currentAnswer}`
