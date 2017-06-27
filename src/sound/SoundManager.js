26d52924318806d399f6bd5f871f4ae6432308b4
solominh <hoangminhftu2@gmail.com>
2017-06-27 19:02:15 +0700

    Reference media by webpack import

--- src/sound/SoundManager.js
+++ src/sound/SoundManager.js
@@ -1,16 +1,18 @@
-
+import touch from '../raw/touch.wav'
+import ding from '../raw/ding.aac'
+import beep from '../raw/beep.aac'
 
 export function playTouchSound() {
-  let sound = new Audio('https://raw.githubusercontent.com/solominh/react-math/master/public/touch.wav')
+  let sound = new Audio(touch)
   sound.play()
 }
 
 export function playRightAnswerSound() {
-  let sound = new Audio('https://raw.githubusercontent.com/solominh/react-math/master/public/ding.aac')
+  let sound = new Audio(ding)
   sound.play()
 }
 
 export function playWrongAnswerSound() {
-  let sound = new Audio('https://raw.githubusercontent.com/solominh/react-math/master/public/beep.aac')
+  let sound = new Audio(beep)
   sound.play()
 }
