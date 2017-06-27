3876c72966f02ab9836c491f05ec593d8c72f5d1
solominh <hoangminhftu2@gmail.com>
2017-06-27 14:25:47 +0700

    Add update database feature

--- src/db/localStorage.js
+++ src/db/localStorage.js
@@ -1,7 +1,35 @@
 
-const DB_VERSION = 1
+const DB_VERSION = 1;
+const DB_VERSION_KEY = 'DB_VERSION';
 
 const getDBKey = (key) => `DB_VERSION_${DB_VERSION}_${key}`;
+
+const checkDB = (version) => {
+  const prevDBVersion = localStorage.getItem(DB_VERSION_KEY)
+
+  // Remove old database
+  if (prevDBVersion && (Number(prevDBVersion) !== DB_VERSION)) {
+    const DB_PREFIX = `DB_VERSION_${prevDBVersion}`
+    const removeKeys = []
+    const len = localStorage.length
+    for (let i = 0; i < len; i++) {
+      const key = localStorage.key(i)
+      if (key.startsWith(DB_PREFIX)) {
+        removeKeys.push(key)
+      }
+    }
+
+    removeKeys.forEach((key) => localStorage.removeItem(key))
+  }
+
+  // Set new data version
+  if (!prevDBVersion || Number(prevDBVersion) !== DB_VERSION) {
+    localStorage.setItem(DB_VERSION_KEY, DB_VERSION)
+  }
+
+}
+
+checkDB(DB_VERSION)
 
 export const loadState = (key) => {
   try {
